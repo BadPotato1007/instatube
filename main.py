40ab8e282bf2d615b2e3876fd496d35ceadc724d
Neerav Gupta <70943638+BadPotato1007@users.noreply.github.com>
2023-11-05 11:36:23 +0000

    changed the way videos are concentrated

--- main.py
+++ main.py
@@ -1,32 +1,41 @@
 from moviepy.editor import VideoFileClip, concatenate_videoclips
 import os
+import random
 
-
+clips = []
 i = 1
-path="./vids/"
+path="./toprocess/"
 for filename in os.listdir(path):
-    my_dest =str(i) + ".mp4"
+    my_dest = "./processed/" + str(i) + ".mp4"
     my_source =path + filename
-    my_dest =path + my_dest
     # rename() function will
     # rename all the files
     os.rename(my_source, my_dest)
     i += 1
-# N = 1
-# F = 1
 
-# # Load the four video clips
-# clip1 = VideoFileClip(N + ".mp4")
-# N = N+1
-# clip2 = VideoFileClip(N + ".mp4")
-# N = N+1
-# clip3 = VideoFileClip(N + ".mp4")
-# N = N+1
-# clip4 = VideoFileClip(N + ".mp4")
-# N = N+1
 
-# # Concatenate the four video clips
-# final_clip = concatenate_videoclips([clip1, clip2, clip3, clip4])
+# filenames = os.listdir("./processed/")  # You could generate this from os.listdir or similar
+# clips = []
 
-# # Write the final clip to a file
-# final_clip.write_videofile("Final " + F + ".mp4")
+# for file in filenames:
+#     clips.append(VideoFileClip(file, target_resolution=(1080, 1920)))
+
+# final_clip = concatenate_videoclips(clips)  # method="chain" by default. This is fine because all clips are the same size
+# final_clip.write_videofile("out.mp4")
+
+N = 1
+F = 1
+# Load the four video clips
+clip1 = VideoFileClip("./processed/" + str(N) + ".mp4", target_resolution=(720, 1280))  # can be 1080, 1920 for full hd
+N = N+1
+clip2 = VideoFileClip("./processed/" + str(N) + ".mp4", target_resolution=(720, 1280))
+N = N+1
+clip3 = VideoFileClip("./processed/" + str(N) + ".mp4", target_resolution=(720, 1280))
+N = N+1
+clip4 = VideoFileClip("./processed/" + str(N) + ".mp4", target_resolution=(720, 1280))
+N = N+1
+# Concatenate the four video clips
+final_clip = concatenate_videoclips([clip1, clip2, clip3, clip4])
+
+# Write the final clip to a file
+final_clip.write_videofile("Final " + str(F) + ".mp4", threads = 4, fps=30)
